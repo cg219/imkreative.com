1daa7fca3876f89665a913cf5d9ea3ce373c61d0
Clemente Gomez <zomegpro@gmail.com>
2017-06-14 02:22:39 -0400

    Add Extended Parser

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let ghostOptions = {
 }
 
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/portfolio', (req, res) => {
